e7bc778fe6e5a6927303ee47f6a80f1e0b72b4fd
mttaskin <0mttaskin1@gmail.com>
2023-03-24 16:01:15 +0300

    v10

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -47,7 +47,7 @@ const icons = [
   {
     title: "Admin Panel",
     icon: <SupervisorAccountIcon />,
-    url: "https://10001.fullstack.clarusway.com/admin",
+    url: "https://12101.fullstack.clarusway.com/admin",
   },
 ]
 
