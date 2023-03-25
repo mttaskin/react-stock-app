43334bec533bb261cc130a3d0ce53fa630798213
mttaskin <0mttaskin1@gmail.com>
2023-03-25 13:48:47 +0300

    v17

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,18 +1,22 @@
-import axios from "axios"
+
 import { useDispatch, useSelector } from "react-redux"
 import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice"
+import useAxios from "./useAxios"
 
 const useStockCall = () => {
   const dispatch = useDispatch()
   const { token } = useSelector((state) => state.auth)
-  
+  const { axiosWithToken } = useAxios()
+
   const getStockData = async (url) => {
-    const BASE_URL = "https://12101.fullstack.clarusway.com/"
+    // const BASE_URL = "https://10001.fullstack.clarusway.com/"
     dispatch(fetchStart())
     try {
-      const { data } = await axios(`${BASE_URL}stock/${url}/`, {
-        headers: { Authorization: `Token ${token}` },
-      })
+      //   const { data } = await axios(`${BASE_URL}stock/${url}/`, {
+      //     headers: { Authorization: `Token ${token}` },
+      //   })
+      const { data } = await axiosWithToken(`stock/${url}/`)
+      console.log(data)
       dispatch(getSuccess({ data, url }))
     } catch (error) {
       console.log(error)
@@ -20,7 +24,18 @@ const useStockCall = () => {
     }
   }
 
-  return { getStockData }
+  const deleteStockData = async (url, id) => {
+    dispatch(fetchStart())
+    try {
+      await axiosWithToken.delete(`stock/${url}/${id}/`)
+      getStockData(url)
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail())
+    }
+  }
+
+  return { getStockData, deleteStockData }
 }
 
 export default useStockCall
