19709d93b931cbaf26ad11bb7917f54c5fafa8ff
mttaskin <0mttaskin1@gmail.com>
2023-03-25 12:19:19 +0300

    v13

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,8 +1,41 @@
-
+import { Button } from "@mui/material"
+import Typography from "@mui/material/Typography"
+import { useEffect } from "react"
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice"
 
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+
+  const getFirms = async () => {
+    const BASE_URL = "https://12101.fullstack.clarusway.com/"
+    dispatch(fetchStart())
+    const url = "firms"
+    try {
+      const { data } = await axios(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      })
+      dispatch(getSuccess({ data, url }))
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail())
+    }
+  }
+
+  useEffect(() => {
+    getFirms()
+  }, [])
+
   return (
-    <div>Firms</div>
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+
+      <Button variant="contained">New Firm</Button>
+    </div>
   )
 }
 
